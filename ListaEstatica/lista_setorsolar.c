a829de99502d7d68d79fe55fb306f0623f4ae36e
gustavo-b <gustavo.hsbatista@gmail.com>
2016-10-03 17:27:16 -0300

    Algoritmos implementados

--- ListaEstatica/lista_setorsolar.c
+++ ListaEstatica/lista_setorsolar.c
@@ -22,7 +22,7 @@
 	3 - Remover setor da lista
 	4 - Consultar setor da lista
 	5 - Exibe a lista
-	6 - Exibir os blocos com baixa eficiencia
+	6 - Exibir os setores com baixa eficiencia
 	7 - Redistribuir de acordo com a eficiencia de cada setor
 	8 - Gerar um relatório do setor
 	9 - Gerar um relatório da lista
@@ -55,103 +55,116 @@ typedef struct {
     int Prim, Ult; //auxiliares para primeira e última posição da lista.
 }Lista_est;
 
-void Criar_Lista_Vazia (Lista_est *Setor_Aux){
-    Setor_Aux->Prim = 0;
-    Setor_Aux->Ult = Setor_Aux->Prim;
+void Criar_Lista_Vazia (Lista_est *Lista_Usina){
+    Lista_Usina->Prim = 0;
+    Lista_Usina->Ult = Lista_Usina->Prim;
 }
 
-int Verifica_Lista_Vazia(Lista_est Setor_Aux){
-    return (Setor_Aux.Prim == Setor_Aux.Ult);
+int Verifica_Lista_Vazia(Lista_est Lista_Usina){
+    return (Lista_Usina.Prim == Lista_Usina.Ult);
 }
 
-int Verifica_Lista_Cheia(Lista_est Setor_Aux){
-	return (Setor_Aux.Ult == MAX);
+int Verifica_Lista_Cheia(Lista_est Lista_Usina){
+	return (Lista_Usina.Ult == MAX);
 }
 
 float Calc_Efic(float geracao){
 	return (((geracao / AREA) / EFIC_MAX) / 10);
 }
 
-void Insere_Elemento_Lista(Lista_est *Setor_Aux, Setor_Painel setor){
+void Insere_Elemento_Lista(Lista_est *Lista_Usina, Setor_Painel setor){
 
 	int p;
 
-	p = Setor_Aux->Prim;
+	p = Lista_Usina->Prim;
 
 
-		if(Verifica_Lista_Cheia(*Setor_Aux)){
+		if(Verifica_Lista_Cheia(*Lista_Usina)){
 			printf("Lista lotada, não pode ser adicionado mais dados");
 		}
 		else {
-			p = Setor_Aux->Prim;
+			p = Lista_Usina->Prim;
 
-			while ((p < Setor_Aux->Ult) && (setor.eficiencia_setor < Setor_Aux->Item[p].eficiencia_setor)) {
+			while ((p < Lista_Usina->Ult) && (setor.eficiencia_setor < Lista_Usina->Item[p].eficiencia_setor)) {
 				p++;
 			}
-			if (p == Setor_Aux->Ult){
-				Setor_Aux->Item[p] = setor;
-				Setor_Aux->Ult++;
+			if (p == Lista_Usina->Ult){
+				Lista_Usina->Item[p] = setor;
+				Lista_Usina->Ult++;
 			}
 			else {
-				if ((setor.eficiencia_setor != Setor_Aux->Item[p].eficiencia_setor)){
+				if ((setor.eficiencia_setor != Lista_Usina->Item[p].eficiencia_setor)){
 					int i;
-					for(i = Setor_Aux->Ult; i > p; i--){
-						Setor_Aux->Item[i] = Setor_Aux->Item[i-1];
+					for(i = Lista_Usina->Ult; i > p; i--){
+						Lista_Usina->Item[i] = Lista_Usina->Item[i-1];
 					}
-					Setor_Aux->Item[p] = setor;
-					Setor_Aux->Ult++;
+					Lista_Usina->Item[p] = setor;
+					Lista_Usina->Ult++;
 				}
 			}
 		}
 }
 
-void Remove_Elemento_Lista(Lista_est *Setor_Aux, Setor_Painel *setor){
+void Remove_Elemento_Lista(Lista_est *Lista_Usina, Setor_Painel *setor){
 
 	int p, i;
 
-	if(Verifica_Lista_Vazia(*Setor_Aux)){
+	if(Verifica_Lista_Vazia(*Lista_Usina)){
 		printf("A lista já está vazia, impossível remover elemento.");
 	}
 	else {
-		p = Setor_Aux->Prim;
+		p = Lista_Usina->Prim;
 
-		while ((p < Setor_Aux->Ult) && (setor->identificacao_setor > Setor_Aux->Item[p].identificacao_setor)) {
+		while ((p < Lista_Usina->Ult) && (setor->identificacao_setor > Lista_Usina->Item[p].identificacao_setor)) {
 			p++;
 		}
-		if (p == Setor_Aux->Ult || (setor->identificacao_setor != Setor_Aux->Item[p].identificacao_setor)){
+		if (p == Lista_Usina->Ult){
 			printf("Setor nao foi encontrado no sistema!\n");
 		}
 		else {
-			*setor = Setor_Aux->Item[p];
+			*setor = Lista_Usina->Item[p];
             printf("Setor removido do sistema!\n");
-			for(i = p; i < Setor_Aux->Ult; i++) {
-				Setor_Aux->Item[i] = Setor_Aux->Item[i + 1];
+			for(i = p; i < Lista_Usina->Ult; i++) {
+				Lista_Usina->Item[i] = Lista_Usina->Item[i + 1];
 			}
-			Setor_Aux->Ult--;
+			Lista_Usina->Ult--;
 		}
 	}
 }
 
-float Get_Random_Float(){
-    double a = 7000.0;
-    float r = ((double)rand()/(double)(RAND_MAX)) * a;
-    return r + 15000.0;
+int Get_Random_Int(int min, int max){
+    int r;
+    const unsigned int range = 1 + max - min;
+    const unsigned int buckets = RAND_MAX / range;
+    const unsigned int limit = buckets * range;
+
+    do{
+        r = rand();
+    } while (r >= limit);
+
+    return min + (r / buckets);
 }
 
-void Ler_Setor(Setor_Painel *A, Lista_est Setor_Aux) {
+float Get_Random_Float(){
+    double a = Get_Random_Int(3000, 10000);
+    float r = ((double)rand()/(double)(RAND_MAX)) * a;
+    return r + Get_Random_Int(10000, 12000);
+}
+
+void Ler_Setor(Setor_Painel *A, Lista_est Lista_Usina) {
 	int i = 0, j;
 	float soma_geracao = 0;
 
 	printf("\nEntre com o codigo identificador de 4 digitos do setor: ");
 	scanf("%d", &A->identificacao_setor);
 
-	while ((i < Setor_Aux.Ult) && (A->identificacao_setor != Setor_Aux.Item[i].identificacao_setor))
+	while ((i < Lista_Usina.Ult) && (A->identificacao_setor != Lista_Usina.Item[i].identificacao_setor))
         i++;
 
-    if(i < Setor_Aux.Ult) {
+    if(i < Lista_Usina.Ult) {
         printf("Codigo de identifcacao ja existe na Lista.");
 
-        return Ler_Setor(&(*A), Setor_Aux);
+        return Ler_Setor(&(*A), Lista_Usina);
     }
 
 	for(i = 0; i < TAM; i++){
@@ -191,12 +204,12 @@ void Ler_Setor(Setor_Painel *A, Lista_est Setor_Aux) {
 	A->eficiencia_setor = Calc_Efic(soma_geracao);
 }
 
-void Exibir_Lista(Lista_est L) {
-	if(Verifica_Lista_Vazia(L)){
+void Exibir_Lista(Lista_est Lista_Usina) {
+	if(Verifica_Lista_Vazia(Lista_Usina)){
 		printf("A lista esta vazia. -Exibe lista\n");
 	}
 	else{
-		int i, j, k = 0, P = L.Prim;
+		int i, j, k = 0, P = Lista_Usina.Prim;
 
 		printf("\n");
 
@@ -212,35 +225,35 @@ void Exibir_Lista(Lista_est L) {
 
 		printf("\n");
 
-		while(P < L.Ult){
-			printf("\t\t\t\t\tSetor %d\t\t\t\t\t\n\n", L.Item[P].identificacao_setor);
-			printf("\t\t\t\tEficiencia Media: %.2f%%\t\t\t\t\n\n", L.Item[P].eficiencia_setor);
+		while(P < Lista_Usina.Ult){
+			printf("\t\t\t\t\tSetor %d\t\t\t\t\t\n\n", Lista_Usina.Item[P].identificacao_setor);
+			printf("\t\t\t\tEficiencia Media: %.2f%%\t\t\t\t\n\n", Lista_Usina.Item[P].eficiencia_setor);
 
 			for(i = 0; i < TAM; i++){
         		for(j = 0; j < TAM; j++){
-					printf("Painel %d", L.Item[P].paineis[i][j].identificacao_painel);
+					printf("Painel %d", Lista_Usina.Item[P].paineis[i][j].identificacao_painel);
 					printf("\t\t");
         		}
 				printf("\n");
 
         		for(j = 0; j < TAM; j++){
 					printf("Ativo: ");
-        		    if(L.Item[P].paineis[i][j].ativo) printf("SIM");
+        		    if(Lista_Usina.Item[P].paineis[i][j].ativo) printf("SIM");
 					else printf("NAO");
 					printf("\t\t");
 				}
         		printf("\n");
 
         		for(j = 0; j < TAM; j++){
-            		printf("Ger.: %.2f W", L.Item[P].paineis[i][j].geracao );
-          			if(L.Item[P].paineis[i][j].geracao > 9999 || L.Item[P].paineis[i][j].geracao < -999) printf("\t");
+            		printf("Ger.: %.2f W", Lista_Usina.Item[P].paineis[i][j].geracao );
+          			if(Lista_Usina.Item[P].paineis[i][j].geracao > 9999 || Lista_Usina.Item[P].paineis[i][j].geracao < -999) printf("\t");
          		   	else printf("\t\t");
 				}
 			    printf("\n");
 
 				for(j = 0; j < TAM; j++){
-    			    printf("Efic.: %.2f%%", L.Item[P].paineis[i][j].eficiencia );
-    			    if(L.Item[P].paineis[i][j].eficiencia > 9999 || L.Item[P].paineis[i][j].eficiencia < -999) printf("\t");
+    			    printf("Efic.: %.2f%%", Lista_Usina.Item[P].paineis[i][j].eficiencia );
+    			    if(Lista_Usina.Item[P].paineis[i][j].eficiencia > 9999 || Lista_Usina.Item[P].paineis[i][j].eficiencia < -999) printf("\t");
     			    else printf("\t\t");
     			}
 			    printf("\n\n");
@@ -265,37 +278,37 @@ void Exibir_Lista(Lista_est L) {
 	}
 }
 
-void Exibir_Setor(Lista_est L, int P){
+void Exibir_Setor(Lista_est Lista_Usina, int P){
     int i, j;
 
-    printf("\t\t\t\t\tSetor %d\t\t\t\t\t\n\n", L.Item[P].identificacao_setor);
-    printf("\t\t\t\tEficiencia Media: %.2f%%\t\t\t\t\n\n", L.Item[P].eficiencia_setor);
+    printf("\t\t\t\t\tSetor %d\n\n", Lista_Usina.Item[P].identificacao_setor);
+    printf("\t\t\t\tEficiencia Media: %.2f%%\n\n", Lista_Usina.Item[P].eficiencia_setor);
 
     for(i = 0; i < TAM; i++){
         for(j = 0; j < TAM; j++){
-            printf("Painel %d", L.Item[P].paineis[i][j].identificacao_painel);
+            printf("Painel %d", Lista_Usina.Item[P].paineis[i][j].identificacao_painel);
             printf("\t\t");
         }
         printf("\n");
 
         for(j = 0; j < TAM; j++){
             printf("Ativo: ");
-            if(L.Item[P].paineis[i][j].ativo) printf("SIM");
+            if(Lista_Usina.Item[P].paineis[i][j].ativo) printf("SIM");
             else printf("NAO");
             printf("\t\t");
         }
         printf("\n");
 
         for(j = 0; j < TAM; j++){
-            printf("Ger.: %.2f W", L.Item[P].paineis[i][j].geracao );
-            if(L.Item[P].paineis[i][j].geracao > 9999 || L.Item[P].paineis[i][j].geracao < -999) printf("\t");
+            printf("Ger.: %.2f W", Lista_Usina.Item[P].paineis[i][j].geracao );
+            if(Lista_Usina.Item[P].paineis[i][j].geracao > 9999 || Lista_Usina.Item[P].paineis[i][j].geracao < -999) printf("\t");
             else printf("\t\t");
         }
         printf("\n");
 
         for(j = 0; j < TAM; j++){
-            printf("Efic.: %.2f%%", L.Item[P].paineis[i][j].eficiencia );
-            if(L.Item[P].paineis[i][j].eficiencia > 9999 || L.Item[P].paineis[i][j].eficiencia < -999) printf("\t");
+            printf("Efic.: %.2f%%", Lista_Usina.Item[P].paineis[i][j].eficiencia );
+            if(Lista_Usina.Item[P].paineis[i][j].eficiencia > 9999 || Lista_Usina.Item[P].paineis[i][j].eficiencia < -999) printf("\t");
             else printf("\t\t");
         }
         printf("\n\n");
@@ -303,20 +316,20 @@ void Exibir_Setor(Lista_est L, int P){
 
 }
 
-void Consultar_Elemento_Setor(Lista_est Setor_Aux, int codigo_setor){
+void Consultar_Elemento_Setor(Lista_est Lista_Usina, int codigo_setor){
     int i;
 
-    if(Verifica_Lista_Vazia(Setor_Aux)) {
+    if(Verifica_Lista_Vazia(Lista_Usina)) {
         printf("A lista está vazia.");
     }
     else {
-        i = Setor_Aux.Prim;
+        i = Lista_Usina.Prim;
 
-        while ((i < Setor_Aux.Ult) && (codigo_setor != Setor_Aux.Item[i].identificacao_setor))
+        while ((i < Lista_Usina.Ult) && (codigo_setor != Lista_Usina.Item[i].identificacao_setor))
             i++;
-        if(i < Setor_Aux.Ult) {
+        if(i < Lista_Usina.Ult) {
 
-            Exibir_Setor(Setor_Aux, i);
+            Exibir_Setor(Lista_Usina, i);
 
         }
 
@@ -324,8 +337,112 @@ void Consultar_Elemento_Setor(Lista_est Setor_Aux, int codigo_setor){
 
 }
 
-void cls(void)
-{
+void Efic_Setores(Lista_est Lista_Usina){
+	int low_effic = 0;
+	
+	if(Verifica_Lista_Vazia(Lista_Usina)) {
+        printf("A lista está vazia.");
+    }
+    
+    int cont = Lista_Usina.Prim;
+    
+    while(cont < Lista_Usina.Ult){
+   		if((Lista_Usina.Item[cont].eficiencia_setor * EFIC_MAX) >= 13){
+   			low_effic++;
+			printf("\t\t\t\t***ABAIXO DA MEDIA DE EFICIENCIA***\n");
+			Exibir_Setor(Lista_Usina, cont);
+			getchar();
+			getchar();
+		}
+		else{
+			if((Lista_Usina.Item[cont].eficiencia_setor * EFIC_MAX) < 13){
+				low_effic++;
+				printf("\t\t\t\t***EFICIENCIA BAIXA***\n");
+				Exibir_Setor(Lista_Usina, cont);
+				getchar();
+				getchar();
+			}
+		}
+		cont++;
+	}
+	
+	if(low_effic == 0){
+		printf("Parabens! Nenhum setor abaixo da producao media esperada!\n");
+	}
+}
+
+void Relatorio_Setor(Lista_est Lista_Usina, Setor_Painel setor){
+	int p;
+	
+	if(Verifica_Lista_Vazia(Lista_Usina)){
+		printf("A lista esta vazia.");
+	}
+	else {
+		p = Lista_Usina.Prim;
+
+		while ((p < Lista_Usina.Ult) && (setor.identificacao_setor > Lista_Usina.Item[p].identificacao_setor)) {
+			p++;
+		}
+		if (p == Lista_Usina.Ult){
+			printf("Setor nao foi encontrado no sistema!\n");
+		}
+		else {
+			int i, j, quant_ativos = 0;
+		    float somaWatts = 0, somaEff = 0, eficienciatotal;
+
+		    for(i = 0; i < TAM; i++){
+    			for(j = 0; j < TAM; j++){
+    				somaWatts += setor.paineis[i][j].geracao;
+        		    if(setor.paineis[i][j].ativo){
+        		    	quant_ativos++;
+					}
+				}
+		    }
+		
+		    printf("\n************RELATORIO GERAL**************\n");
+		    printf("*  \t\t\t\t\t*\n*\t\t\t\t\t*\n");
+		    printf("*  Paineis Ativados: %d\t\t\t*\n", quant_ativos);
+		    printf("*  Paineis Desativados: %d\t\t*\n", (TAM * TAM) - quant_ativos );
+		    if(somaWatts > 999999){
+		    	printf("*  Total Energ. Ger.: %.2f\t*\n", somaWatts);
+			}
+			else{
+				printf("*  Total Energ. Ger.: %.2f\t\t*\n", somaWatts);
+			}
+			if((somaWatts/(TAM*TAM)) > 999999){
+		    	printf("*  Media Energ. Ger.: %.2f\t*\n", somaWatts / (TAM*TAM)); //a média é em relação todos os paineis, até os desativados
+			}
+			else{
+				printf("*  Media Energ. Ger.: %.2f\t\t*\n", somaWatts / (TAM*TAM)); //a média é em relação todos os paineis, até os desativados
+			}
+		    printf("*  Media Eficiencia: %.2f%%\t\t*\n", setor.eficiencia_setor);
+		    if((setor.eficiencia_setor * EFIC_MAX) >= 16){
+		    	printf("*  EFICIENCIA ALTA\t\t\t*\n");
+			}
+			else{
+				if((setor.eficiencia_setor * EFIC_MAX) >= 15){
+					printf("*  ACIMA DA MEDIA DE EFICIENCIA\t\t*\n");
+				}
+				else{
+					if((setor.eficiencia_setor * EFIC_MAX) >= 14){
+						printf("*  EFICIENCIA MEDIA\t\t\t*\n");
+					}
+					else{
+						if((setor.eficiencia_setor * EFIC_MAX) >= 13){
+							printf("*  ABAIXO DA MEDIA DE EFICIENCIA\t\t*\n");
+						}
+						else{
+							printf("*  EFICIENCIA BAIXA\t\t\t*\n");
+						}
+					}
+				}
+			}
+		    printf("*  \t\t\t\t\t*\n*\t\t\t\t\t*\n*****************************************\n");
+		}
+	}
+}
+
+void cls(void){
     printf("\e[H\e[2J");
 
     #ifdef LINUX
@@ -341,9 +458,9 @@ void cls(void)
 
 }
 
-int Menu (int index, Lista_est *Setor_Aux, Setor_Painel Setor){
+int Menu (int index, Lista_est *Lista_Usina, Setor_Painel *Setor){
 
-    int escolha, erros = 0, opcao, i, j;
+    int escolha, erros = 0, opcao;
 
     switch (index) {
 
@@ -352,28 +469,28 @@ int Menu (int index, Lista_est *Setor_Aux, Setor_Painel Setor){
 			return 0;
 
 		case 1:
-            Ler_Setor(&Setor, *Setor_Aux);
-            Insere_Elemento_Lista(*(&Setor_Aux), Setor);
+            Ler_Setor(&(*Setor), *Lista_Usina);
+            Insere_Elemento_Lista(&(*Lista_Usina), *Setor);
             break;
 
 		case 2:
 		    printf("Digite a identificacao do setor a ser removido: ");
 		    scanf("%d", &opcao);
-		    Setor.identificacao_setor = opcao;
-		    Remove_Elemento_Lista(*(&Setor_Aux), (&Setor));
+		    Setor->identificacao_setor = opcao;
+		    Remove_Elemento_Lista(&(*Lista_Usina), &(*Setor));
 
 			break;
 
 		case 3:
 
-		    Exibir_Lista(*Setor_Aux);
+		    Exibir_Lista(*Lista_Usina);
 
 			break;
 
 		case 4:
 		    printf("Digite a identificacao do setor a ser consultado: ");
 		    scanf("%d", &opcao);
-		    Consultar_Elemento_Setor(*Setor_Aux, opcao);
+		    Consultar_Elemento_Setor(*Lista_Usina, opcao);
 
 		    break;
 
@@ -386,12 +503,18 @@ int Menu (int index, Lista_est *Setor_Aux, Setor_Painel Setor){
 			break;
 
 		case 7:
-
-			break;
-
-        case 8:
-
+			Efic_Setores(*Lista_Usina);
             break;
+            
+        case 8:
+        	printf("Digite a identificacao do setor a ser gerado relatorio: ");
+		    scanf("%d", &opcao);
+        	Relatorio_Setor(*Lista_Usina, *Setor);
+        	break;
+        	
+        case 9:
+        	
+        	break;
 
 //		Case default para tentativas falsas.
 		default:
@@ -405,11 +528,11 @@ int Menu (int index, Lista_est *Setor_Aux, Setor_Painel Setor){
 
 int main ( ) {
 	int index = 1, erros = 0, aux;
-	Lista_est Setor_Aux;
+	Lista_est Lista_Usina;
 	Setor_Painel Setor;
 	srand((unsigned int)time(NULL));
 
-	Criar_Lista_Vazia(&Setor_Aux);
+	Criar_Lista_Vazia(&Lista_Usina);
 
     printf("************Bem vindo ao Apolo Manager************\n\n");
 
@@ -422,16 +545,17 @@ int main ( ) {
 		printf("1 - Inserir Setor no Sistema\n"); //implementado
 		printf("2 - Remover Setor do Sistema\n"); //implementado
 		printf("3 - Exibir Lista\n"); //implementado
-		printf("4 - Consultar Setor no Sistema\n"); //
+		printf("4 - Consultar Setor no Sistema\n"); //implementado
 		printf("5 - Consultar Painel no Setor\n"); //
-		printf("6 - Consultar Setores mais Proximos\n"); //
-		printf("7 - Buscar elementos em uma matriz\n"); //
-		printf("8 - Relatorio Geral\n\n"); //
+		printf("6 - Consultar Setores mais Proximos\n"); //ou realocar energia para setores?
+		printf("7 - Setores abaixo da eficiencia media\n"); //implementado
+		printf("8 - Relatorio de um Setor\n"); //implementado
+		printf("9 - Relatorio Geral\n\n"); //
 		printf("=============================================\n");
 
 		scanf("%d", &index);
 
-        aux = Menu(index, &Setor_Aux, Setor);
+        aux = Menu(index, &Lista_Usina, &Setor);
 
         if(aux){
             erros += aux;
